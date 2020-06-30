2d2a491eae15957bf04aa0f42d7ab947d5d43f23
belozercevaa <belozerceva.a@yandex.ru>
2020-06-30 18:54:11 +0300

    implemented tasks from lesson 24

--- src/index.js
+++ src/index.js
@@ -7,61 +7,65 @@ let personalMovieDB = {
     actors: '',
     genres: [],
     privat: false,
-};
-
-
-function start() {
-    personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    console.log(personalMovieDB.count);
-    while (isNaN(personalMovieDB.count) || personalMovieDB.count === '' || personalMovieDB.count === null) {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?');
-    }
-
-    detectPersonalLevel(personalMovieDB.count)
-}
-
-function detectPersonalLevel(count) {
-    if (count < 10) {
-        console.log('Просмотренно довольно мало фильмов');
-    } else if (count > 10 && count < 50) {
-        console.log('Вы классический зритель');
-    } else if (count > 50) {
-        console.log('Вы киноман');
-    }
-}
-
-
-function rememberMyFilms() {
-    if (personalMovieDB.count > 0) {
-        for (let i = 0; i < personalMovieDB.count; i++) {
-            let title = prompt('Один из последних посмотренных фильмов?');
-            while (title === '' || title === null || title.length > 50) {
-                title = prompt('Один из последних посмотренных фильмов?');
-            }
-            let stars = prompt('На сколько оцените его?');
-            while (stars === '' || stars === null) {
-                stars = prompt('На сколько оцените его?');
-            }
-
-            personalMovieDB.movies[title] = stars;
+    countMovies: function() {
+        this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        while (isNaN(this.count) || this.count === '' || this.count === null) {
+            this.count = +prompt('Сколько фильмов вы уже посмотрели?');
         }
-    }
+
+        this.detectPersonalLevel()
+    },
+    detectPersonalLevel: function() {
+        if (this.count < 10) {
+            console.log('Просмотренно довольно мало фильмов');
+        } else if (this.count > 10 && this.count < 50) {
+            console.log('Вы классический зритель');
+        } else if (this.count > 50) {
+            console.log('Вы киноман');
+        }
+    },
+    rememberMyFilms: function() {
+        if (this.count > 0) {
+            for (let i = 0; i < this.count; i++) {
+                let title = prompt('Один из последних посмотренных фильмов?');
+                while (title === '' || title === null || title.length > 50) {
+                    title = prompt('Один из последних посмотренных фильмов?');
+                }
+                let stars = prompt('На сколько оцените его?');
+                while (stars === '' || stars === null) {
+                    stars = prompt('На сколько оцените его?');
+                }
+
+                this.movies[title] = stars;
+            }
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genres = prompt(`Ваш любимый жанр под номером ${i}`);
+            while (genres === '' || genres === null) {
+                genres = prompt(`Ваш любимый жанр под номером ${i}`);
+            }
+            this.genres.push(genres);
+        }
+        this.genres.forEach((value, i) => {
+            console.log(`Любимый жанр ${++i} - это ${value}`)
+        });
+    },
+    showMyDB: function() {
+        if (!this.privat) {
+            console.log(this);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        this.privat = !this.privat;
+    },
 };
 
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        let genres = prompt(`Ваш любимый жанр под номером ${++i}`);
-        personalMovieDB.genres.push(genres);
-    }
-}
 
-function showMyDB(privat) {
-    if (!privat) {
-        console.log(personalMovieDB);
-    }
-}
-
-start();
-rememberMyFilms();
-writeYourGenres();
-showMyDB(personalMovieDB.privat);
+// personalMovieDB.countMovies();
+// personalMovieDB.rememberMyFilms();
+personalMovieDB.writeYourGenres();
+personalMovieDB.showMyDB();
+// personalMovieDB.toggleVisibleMyDB();
+// personalMovieDB.showMyDB();
